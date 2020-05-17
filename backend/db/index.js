3e8300772a315f970d381cd8b75859d0fde47afa
maria liza <mariamartinez@pursuit.org>
2020-05-17 16:11:08 -0400

    complete env set up for backend

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const pgp = require('pg-promise')({});
+const pgp = require("pg-promise")({});
+
 const db = pgp(process.env.DATABASE_URL);
+
 module.exports = db;
