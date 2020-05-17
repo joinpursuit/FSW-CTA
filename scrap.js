7a048c27d175a326b6e4b2c112930203d0b028a3
maria liza <mariamartinez@pursuit.org>
2020-05-17 15:30:17 -0400

    begin set up

---
+++ scrap.js
@@ -0,0 +1,22 @@
+// Choose a site from the following list and build a copy of it. Each site has its own unique set of features that you should try to build, which are listed below. All of these sites also have authentication, so assume that "user" means someone who has created an account and logged in.
+
+// - Twitter
+//   - Homepage that shows all tweets
+//   - User page that shows a user's tweets
+//   - A form to write new tweets with a 280    character limit
+//   - A way to search tweets by #hashtags
+//   - Bonus: add liking tweets / retweet functionality
+//   - Bonus: Ability to upload a photo with tweet
+
+//////////////////////////////////////////////////
+
+//HOME
+    //?like single tweet?
+    //??retweet??
+//NAVBAR
+    //search tweets by #
+//FOOTER
+//USER - shows tweets/media/profile pic/ small bio
+    //form for new tweets with 280 char limit
+    //uplaod pic on tweet
+ 
