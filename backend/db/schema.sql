79f64a39b8c401d9b824e34a85f4217e0b8d04aa
maria liza <mariamartinez@pursuit.org>
2020-05-17 15:49:26 -0400

    create db, schema, seeds, connect to psql

---
+++ backend/db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS twitter_database;
+CREATE DATABASE twitter_database;
+
+\c twitter_database;
+
+/* 
+
+-users table
+-tweets table
+
+*/
---
+++ backend/db/seeds.sql
@@ -0,0 +1,7 @@
+-- \c twitter_database; disabled for heroku check/update
+
+/*
+insert into tables here for generic testing info 
+*/
+
+
